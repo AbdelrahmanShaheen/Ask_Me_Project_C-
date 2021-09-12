d700848e25a0293e88d61d692400a049e07e67d2
AbdelrahmanShaheen <77184432+AbdelrahmanShaheen@users.noreply.github.com>
2021-09-12 11:47:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main()
     AskSystem *service;
     service = AskSystem::GetInstance();
     service->run();
+    service->FreeInstance();
 }
 // Note i am using singleton design pattern to provide only one object of AskSystem.
